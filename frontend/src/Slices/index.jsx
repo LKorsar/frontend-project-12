b7e1afdcf0ab1f4d92f1b04cf6969b341b95917d
Мария Акимкина <wapovalova@gmail.com>
2025-07-21 18:04:17 +0300

    updated frontend/src/Slices/index.jsx

--- frontend/src/Slices/index.jsx
+++ frontend/src/Slices/index.jsx
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice.jsx';
 import channelsReducer from './channelsSlice.jsx';
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     authReducer,
     channelsReducer,
   },
 });
+
+export default store;
