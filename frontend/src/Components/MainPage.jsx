3250dbda809dd7baddf49d4d8887a2d4468db85f
Мария Акимкина <wapovalova@gmail.com>
2025-07-22 13:45:44 +0300

    fixed errors

--- frontend/src/Components/MainPage.jsx
+++ frontend/src/Components/MainPage.jsx
@@ -96,7 +96,7 @@ const MainPage = () => {
                   <div className="d-flex flex-column h-100">
                     <div className="bg-light mb-4 p-3 shadow-sm small">
                       <p className="m-0">
-                        <b>{`# ${channels[0].name}`}</b>
+                        <b>#</b>
                       </p>
                       <span className="text-muted">0 сообщений</span>
                     </div>
