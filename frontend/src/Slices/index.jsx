0eeb2e27c2f9911bfcfd242903788c8c34df377c
Мария Акимкина <wapovalova@gmail.com>
2025-07-22 11:58:41 +0300

    fixed errors

--- frontend/src/Slices/index.jsx
+++ frontend/src/Slices/index.jsx
@@ -2,11 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './authSlice.jsx';
 import channelsReducer from './channelsSlice.jsx';
 
-const store = configureStore({
+export default configureStore({
   reducer: {
     authReducer,
     channelsReducer,
   },
 });
-
-export default store;
