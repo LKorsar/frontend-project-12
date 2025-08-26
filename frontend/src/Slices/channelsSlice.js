56296686b7cf0d4e9014ff8fc5a2fd7c2e58b3ab
Мария Акимкина <wapovalova@gmail.com>
2025-08-26 17:26:09 +0300

    fixed error in activeChannel

--- frontend/src/Slices/channelsSlice.js
+++ frontend/src/Slices/channelsSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   channels: [],
-  activeChannel: { name: 'general', id: 1, removable: false },
+  activeChannel: { name: 'general', id: '1', removable: false },
 };
 const channelsSlice = createSlice({
   name: "channels",
