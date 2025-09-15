bcf7cfaf3004d924d1c9a5718913e3673a9be875
Мария Акимкина <wapovalova@gmail.com>
2025-09-15 12:56:55 +0300

    fixed linter errors

--- frontend/src/Components/MainPage.jsx
+++ frontend/src/Components/MainPage.jsx
@@ -83,7 +83,6 @@ const MainPage = () => {
     try {
       await renameChannel({ id: id, name: newName })
       refetchChannels()
-      console.log(channels);
       toast.success(t('notifications.chEdited'))
     }
     catch (err) {
